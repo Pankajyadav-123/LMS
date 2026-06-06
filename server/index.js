c6635a25f22b8e78f677621aeb8b3dea05a53ce0
Pankajyadav-123 <Pankajyadav95834@gmail.com>
2026-06-06 12:22:12 +0530

    add more function

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://lms-orcin-rho.vercel.app/" || "http://localhost:5173",
     credentials: true
 }));
  
