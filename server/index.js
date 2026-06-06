2b93a17eadf7462502646407d1d8fec3b31ffd53
Pankajyadav-123 <Pankajyadav95834@gmail.com>
2026-06-06 11:59:21 +0530

    fix error

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
 }));
  
 // apis
